f97c5f5d218c1d5ae901c01dbb2a78abb8cf474a
jokerest007 <jokerest52@gmail.com>
2026-08-18 19:12:30 +0800

    Create welcome.php with user input forms

---
+++ welcome.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE HTML>
+<html>  
+<body>
+
+<form action="welcome_get.php" method="get">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+Password: <input type="text" name="password"><br>
+<input type="submit">
+<input type="reset" value="Reset">
+</form><br>
+
+<form>
+  <input type="radio" id="html" name="fav_language" value="Student">
+  <label for="html">HTML</label><br>
+  <input type="radio" id="css" name="fav_language" value="Graduate">
+  <label for="css">CSS</label><br>
+  <input type="radio" id="javascript" name="fav_language" value="Instructor">
+  <label for="javascript">JavaScript</label>
+</form> 
+
+</body>
+</html>
